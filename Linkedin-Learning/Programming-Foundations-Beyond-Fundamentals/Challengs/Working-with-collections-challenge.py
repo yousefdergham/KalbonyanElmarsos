e5cae9614eb4480d5ecd62dae4ae9a4d08742789
Yousef Dergham <79238462+yousefdergham@users.noreply.github.com>
2022-01-31 23:10:13 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,7 @@
 
+# make app that covert miles to kelometers
+miles = input('Enter a distance in miles: ')
+miles_float = float(miles)
+kilometers = miles_float * 1.609344
+print('That value in kelometers is')
+print(kilometers)
