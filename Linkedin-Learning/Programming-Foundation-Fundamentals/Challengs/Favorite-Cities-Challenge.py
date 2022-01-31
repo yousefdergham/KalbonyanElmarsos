8a0fef73fe736d13c84f89656e5e90e8c29c6d0c
Yousef Dergham <79238462+yousefdergham@users.noreply.github.com>
2022-01-31 23:06:38 +0200

    Update Favorite-Cities-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,8 @@
+#Make a program that prints out the name of your favorite cities using function 
 
+def favorite_city(cite):
+    print("One of my favorite cities is", cite)
+
+favorite_city("Cairo, Egypt")
+favorite_city("Berlin, Germany")
+favorite_city("London, United kingdom")
