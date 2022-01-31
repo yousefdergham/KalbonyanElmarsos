4d19cf5b32b333c9608a371ca3fabd18e985fa3f
Yousef Dergham <79238462+yousefdergham@users.noreply.github.com>
2022-01-31 23:07:14 +0200

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,11 @@
+#make Make a program that ask you what is your favorite food? and replay to you
 
+FavFood = input("What's my favorite food?")
+print(FavFood)
+
+if FavFood == "Chicken":
+    print("Yep! So Tasty!")
+else :
+    print("Yuck!")
+
+print("Thanks for playing!")
